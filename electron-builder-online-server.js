81de2c3538400758ad84b3add87a8e460f702255
paulorieck <paulo.rieck@prodgectool.com>
2019-08-26 06:34:47 -0300

    Development continuation

--- electron-builder-online-server.js
+++ electron-builder-online-server.js
@@ -281,7 +281,7 @@ wss.on('connection', (socket, req) => {
                 console.log(minimist_parameters);
 
                 // Store on nedb project information to process when compiler is unocupied
-                requests_historic.insert(parameters, function (error, newDoc) {
+                requests_historic.insert(minimist_parameters, function (error, newDoc) {
 
                     if ( error ) {
                         console.log("Error:");
