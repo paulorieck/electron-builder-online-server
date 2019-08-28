ddc2e22c4300ffd0748d830d726dec2cedd751b4
paulorieck <paulo.rieck@prodgectool.com>
2019-08-28 12:43:41 -0300

    Development continuation

--- electron-builder-online-server.js
+++ electron-builder-online-server.js
@@ -90,14 +90,14 @@ function processCode(message, ws, socket, this_system, this_color, callback) {
     console.log("processCode ==> message: ");
     console.log(message);
 
-    c1 = parseFloat(message.charAt(message.length()-1));
+    c1 = parseFloat(message.charAt(message.length-1));
     try {
-        c2 = parseFloat(message.charAt(message.length()-2));
+        c2 = parseFloat(message.charAt(message.length-2));
         try {
-            c3 = parseFloat(message.charAt(message.length()-3));
+            c3 = parseFloat(message.charAt(message.length-3));
         } catch (Err) {}
         try {
-            c4 = parseFloat(message.charAt(message.length()-4));
+            c4 = parseFloat(message.charAt(message.length-4));
         } catch (Err) {}
     } catch (Err) {}
     
